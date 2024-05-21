f1f42c778916e87f5f0c2e5cb312236b6a351506
Matthew Steeples <matthew@ledgerscope.com>
2024-05-21 20:32:47 +0100

    Simplest example

---
+++ Constants.cs
@@ -0,0 +1,30 @@
+﻿namespace MVC2
+{
+    public static class Constants
+    {
+        public static Inner Inner = new Inner();   
+    }
+
+    public class Inner
+    {
+        public string Value = "Hello";
+    }
+}
+
+namespace ThreeHundredRepro
+{
+    public static class MVC
+    {
+        public static Constants2 Constants2 => new Constants2();
+    }
+
+    public class Constants2
+    {
+        public Inner Inner = new Inner();
+    }
+
+    public class Inner
+    {
+        public string _value = "Hello";
+    }
+}
--- WarningModal.cs
+++ WarningModal.cs
@@ -2,6 +2,17 @@
 {
     public class WarningModal
     {
+        public WarningModal(string modalId, string modalTitle, string message)
+        {
+            ModalId = modalId;
+            ModalTitle = modalTitle;
+            Message = message;
+        }
+
+        public WarningModal()
+        {
+        }
+
         public string ModalId { get; set; }
         public string ModalTitle { get; set; }
         public string Message { get; set; }
